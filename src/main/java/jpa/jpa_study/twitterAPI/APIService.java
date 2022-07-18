f95e4f825fe04a2bbd828470a52765870a6544a0
QuiD-0 <wodnd101@naver.com>
2022-07-18 17:57:44 +0900

    트위터 api 필터링

--- src/main/java/jpa/jpa_study/twitterAPI/APIService.java
+++ src/main/java/jpa/jpa_study/twitterAPI/APIService.java
@@ -37,7 +37,7 @@ public class APIService {
 
     public void TestTwitterLoading() {
         LocalDateTime endLocalDateTime = LocalDateTime.now();
-        LocalDateTime startLocalDateTime = endLocalDateTime.minusDays(7);
+        LocalDateTime startLocalDateTime = endLocalDateTime.minusDays(365);
         AdditionalParameters additionalParameters = AdditionalParameters.builder().startTime(startLocalDateTime).endTime(endLocalDateTime).build();
 
         UserV2 userV2 = twitterClient.getUserFromUserName("TicketOpen"); //@를 제외한 아이디
@@ -48,10 +48,21 @@ public class APIService {
         System.out.println("트윗 로딩 체크");
         System.out.println("가져온 트윗 수 : " + tweetList.getData().size());
         for (TweetV2.TweetData tweet : tweetList.getData()) {
-            System.out.println("Id : " + tweet.getId());
-            System.out.println("text : " + tweet.getText());
-            System.out.println("==============================");
+            if(tweet.getText().contains("블루스퀘어")){
+                if(tweet.getText().contains("콘서트") || tweet.getText().contains("팬미팅")||tweet.getText().contains("어워즈")) {
+                    continue;
+                }
+                System.out.println("Id : " + tweet.getId());
+                System.out.println("text : " + tweet.getText());
+                System.out.println("==============================");
+            }else{
+                if(!tweet.getText().contains("뮤지컬") && !tweet.getText().contains("연극")&& !tweet.getText().contains("극단"))
+                    continue;
+                System.out.println("Id : " + tweet.getId());
+                System.out.println("text : " + tweet.getText());
+                System.out.println("==============================");
+            }
+
         }
     }
 }
-
