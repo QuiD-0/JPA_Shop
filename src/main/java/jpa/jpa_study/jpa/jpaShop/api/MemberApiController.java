df26d7ae64982388cdf96dd8f8d47647bc982b66
QuiD <wodnd101@naver.com>
2022-08-09 23:25:15 +0900

    엔티티 외부 노출 금지
    엔티티는 DTO를 통해 입출력 하라

--- src/main/java/jpa/jpa_study/jpa/jpaShop/api/MemberApiController.java
+++ src/main/java/jpa/jpa_study/jpa/jpaShop/api/MemberApiController.java
@@ -5,10 +5,11 @@ import jpa.jpa_study.jpa.jpaShop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,14 +18,30 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/v1/member")
-    public CreateMemberResponse saveMember(@RequestBody @Validated Member member){
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    // 엔티티를 외부에 노출하지 않는게 좋음
+    @PostMapping("/api/v2/member")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
     @Data
+    static class CreateMemberRequest{
+        private String name;
+    }
+
+
+    @Data
     @AllArgsConstructor
-    static class CreateMemberResponse{
+    static class CreateMemberResponse {
+
         private Long id;
     }
 }
