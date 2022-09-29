db055a80b0a8dcd36d40b7188abae6d4b734cbee
QuiD-0 <wodnd101@naver.com>
2022-09-29 16:54:17 +0900

    test code

--- src/test/java/jpa/jpa_study/board/repository/JpaTest.java
+++ src/test/java/jpa/jpa_study/board/repository/JpaTest.java
@@ -1,6 +1,7 @@
 package jpa.jpa_study.board.repository;
 
 import jpa.jpa_study.board.config.JpaConfig;
+import jpa.jpa_study.board.domain.Article;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,14 +16,21 @@ import org.springframework.context.annotation.Import;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class JpaTest {
 
-    @Autowired
-    ArticleRepository articleRepository;
+  @Autowired
+  ArticleRepository articleRepository;
 
-    @Test
-    @DisplayName("select 테스트")
-    void testCode() {
-        Assertions.assertThatCode(()-> articleRepository.findAll()).doesNotThrowAnyException();
+  @Test
+  @DisplayName("select 테스트")
+  void select_test() {
+    Assertions.assertThatCode(() -> articleRepository.findAll()).doesNotThrowAnyException();
+  }
 
-    }
+  @Test
+  @DisplayName("insert 테스트")
+  void insert_test() {
+    Article article = Article.builder().title("test").content("test contents").hashtag("#jpa").build();
+    Assertions.assertThatCode(() -> articleRepository.save(article)).doesNotThrowAnyException();
+  }
+
 
 }
