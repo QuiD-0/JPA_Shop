295d5ccc74798335e0bb1d8a08e3171f878905a8
QuiD-0 <wodnd101@naver.com>
2022-10-04 17:29:36 +0900

    queryDsl + data rest 적용

---
+++ src/main/generated/jpa/jpa_study/board/comment/domain/QArticleComment.java
@@ -0,0 +1,66 @@
+package jpa.jpa_study.board.comment.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QArticleComment is a Querydsl query type for ArticleComment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QArticleComment extends EntityPathBase<ArticleComment> {
+
+    private static final long serialVersionUID = 635003360L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QArticleComment articleComment = new QArticleComment("articleComment");
+
+    public final jpa.jpa_study.board.article.domain.QBaseFields _super = new jpa.jpa_study.board.article.domain.QBaseFields(this);
+
+    public final jpa.jpa_study.board.article.domain.QArticle article;
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final StringPath hashtag = createString("hashtag");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public QArticleComment(String variable) {
+        this(ArticleComment.class, forVariable(variable), INITS);
+    }
+
+    public QArticleComment(Path<? extends ArticleComment> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QArticleComment(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QArticleComment(PathMetadata metadata, PathInits inits) {
+        this(ArticleComment.class, metadata, inits);
+    }
+
+    public QArticleComment(Class<? extends ArticleComment> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.article = inits.isInitialized("article") ? new jpa.jpa_study.board.article.domain.QArticle(forProperty("article")) : null;
+    }
+
+}
+
