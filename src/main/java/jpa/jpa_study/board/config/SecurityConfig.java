90a53d0e9114dafe2c6c47e6532364288ad30969
QuiD-0 <wodnd101@naver.com>
2022-10-06 16:05:00 +0900

    SpringSecurity allow post

--- src/main/java/jpa/jpa_study/board/config/SecurityConfig.java
+++ src/main/java/jpa/jpa_study/board/config/SecurityConfig.java
@@ -11,7 +11,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .formLogin().and()
+            .formLogin().and().csrf().disable()
             .build();
+
     }
 }
